46282f19e0beb8c15d02f507b59f4dbbcb5c9621
Bharath Kumar <bharath6365@gmail.com>
2024-04-01 11:13:57 +0530

    feat: remove docker file

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_PROXY_DOMAIN = 'http://app.phonepe.zm1'
+export const DEFAULT_PROXY_DOMAIN = 'http:/yourdomain.com'
 
 export const NUMERIC_REGEX = /^([0-9]+|[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})$/i;
 
